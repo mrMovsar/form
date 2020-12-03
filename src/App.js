d8d8ef2b1e917b79f8d5eef64c33df280cc0b071
Movsar <movsar1991.mb@gmail.com>
2020-12-03 08:52:54 +0300

    frm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [check, setCheck] = useState(false);
+  const [text, setText] = useState("");
+  const [todo, setTodo] = useState([
+    {title:"mahram", completed: true},
+    {title:"general", completed: true},
+  ])
+  const handleCheck = () => {
+  setCheck(!check);
+}
+  const handleChange = (e) =>{
+    setText(e.target.value)
+  }
+  const handleReset = () =>{
+    setText("");
+    setTodo([]);
+  }
+  const handleAdd = () =>{
+    setTodo([{title: text, completed: check}, ...todo])
+    setText("");
+    setCheck(false);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="checkbox"
+      checked={check}
+      onChange={handleCheck}
+     />
+     <input
+     type="text"
+     value={text}
+     onChange={handleChange}
+     placeholder="Введите текст"
+     />
+     <button onClick={handleReset}>Reset</button>
+     <button onClick={handleAdd}>Add</button>
+     <div>
+       {todo.map((prod) => {
+         return (
+           <div>
+              <input type="checkbox" checked={prod.completed}/>
+             {prod.title}
+             </div>
+         )
+       })}
+     </div>
     </div>
   );
 }

cb28bd78185dbeb0bb331e5730ffeb87691d0ee1
Movsar <movsar1991.mb@gmail.com>
2020-12-21 13:55:34 +0300

    promice

--- src/App.js
+++ src/App.js
@@ -1,53 +1,34 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
-  const [check, setCheck] = useState(false);
-  const [text, setText] = useState("");
-  const [todo, setTodo] = useState([
-    {title:"mahram", completed: true},
-    {title:"general", completed: true},
-  ])
-  const handleCheck = () => {
-  setCheck(!check);
-}
-  const handleChange = (e) =>{
-    setText(e.target.value)
-  }
-  const handleReset = () =>{
-    setText("");
-    setTodo([]);
-  }
-  const handleAdd = () =>{
-    setTodo([{title: text, completed: check}, ...todo])
-    setText("");
-    setCheck(false);
-  }
+  const [mahr, setMahr] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos").then((response) => {
+      response.json().then((json) => {
+        setMahr(json);
+    })
+  })
+  }, []);
   
+  
+ const m = mahr.filter(item => item.id<=50)
+  const t = m.map((item) => {
+    const todoclass = item.completed?"todoselected" : "todo";
+    return(
+      <table border="1">
+        <tr>
+          <td className="tab">{item.id}</td>
+          <td  className={todoclass}>{item.title}</td>
+        </tr>
+        
+      </table>
+    )
+  })  
   return (
-    <div className="App">
-      <input type="checkbox"
-      checked={check}
-      onChange={handleCheck}
-     />
-     <input
-     type="text"
-     value={text}
-     onChange={handleChange}
-     placeholder="Введите текст"
-     />
-     <button onClick={handleReset}>Reset</button>
-     <button onClick={handleAdd}>Add</button>
-     <div>
-       {todo.map((prod) => {
-         return (
-           <div>
-              <input type="checkbox" checked={prod.completed}/>
-             {prod.title}
-             </div>
-         )
-       })}
-     </div>
+    <div>
+      <h1>...JSON</h1> 
+      {t}
     </div>
   );
 }
